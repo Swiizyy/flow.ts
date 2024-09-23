3fdf57fa7024533c980d451a565a98647a1ea0c5
Williams Bussat <github@wbussat.fr>
2024-09-23 08:21:17 +0200

    chore: add esliint and prettier config files

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import pluginJs from "@eslint/js";
+import tailwind from "eslint-plugin-tailwindcss";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  // Typescript
+  ...tseslint.configs.recommended,
+  // Tailwind
+  ...tailwind.configs["flat/recommended"],
+  // Vue
+  ...pluginVue.configs["flat/recommended"],
+  // Rules config
+  {
+    rules: {
+      "vue/require-default-prop": 0,
+      "vue/multi-word-component-names": 0,
+      "@typescript-eslint/ban-types": 0,
+      "tailwindcss/no-custom-classname": 0,
+      "@typescript-eslint/no-unused-vars": [
+        1,
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "props",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  {},
+  // Ignore files
+  {
+    ignores: ["tailwind.config.js", "nuxt.config.ts", "*.js"],
+  },
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxt/eslint"],
   postcss: {
     plugins: {
       tailwindcss: {},
